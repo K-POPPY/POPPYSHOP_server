5b811cd9bedd60f65096b2fd9f4678520e5c8f2f
K-POPPY <yeareung-poppy.gmail.com>
2022-07-16 23:00:26 +0900

    상품업로드구현,배너설정 완료

--- server.js
+++ server.js
@@ -1,98 +1,110 @@
-// 라우팅은 애플리케이션 엔드 포인트(URI)의 정의, 그리고 URI가 클라이언트 요청에 응답하는 방식
-const express = require("express"); // express 불러오기
-const cors = require('cors'); // cors 불러오기
-const app = express(); // 불러온 express 실생
-const models = require('./models'); // ./models의 함수들을 불러온다.
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const models = require("./models");
+const multer = require("multer");
+const upload = multer({
+    storage: multer.diskStorage({
+        destination: function (req, file, cb) {
+            cb(null, "uploads");
+        },
+        filename: function (req, file, cb) {
+            cb(null, file.originalname)
+        },
+    }),
+});
 const port = 8080;
 
-//상수 app에
-app.use(express.json()); //json 형식의 데이터를 처리할 수 있게 설정하는 코드
-app.use(cors()) //브라우저의 CORS 이슈를 막기 위해 사용하는 코드
 
-// 다음의 라우트 경로는 요청을 루트 라우트 /에 일치시킴
-app.get('/products', (req, res) => {
+app.use(express.json());
+app.use(cors());
+app.use("/uploads", express.static("uploads"));
+
+app.get("/products", function (req, res) {
     models.Product.findAll({
-        /* 페이지 갯수 지정
-        limit:1 */
-        // 목록 정렬
-        order:[["createdAt","DESC"]], // 내림차순(최신순) DESC 오름차순 ASC
-        attributes:["id","name","price","seller","description","imageUrl","createdAt"]
-        
+        order: [["createdAt", "DESC"]],
+        attributes: ["id", "name", "price", "seller", "imageUrl", "createdAt"],
     })
         .then((result) => {
-            console.log("PRODUCTS:", result);
             res.send({
                 product: result,
             });
         })
         .catch((err) => {
             console.error(err);
-            res.send("에러발생");
+            res.status(400).send("에러발생");
         });
 });
 
-app.get('/products/:id', (req, res) => {
+app.get("/products/:id", function (req, res) {
     const params = req.params;
     const { id } = params;
     models.Product.findOne({
         where: {
-            // id: id,
             id,
         },
     })
-        .then((result)=>{
-            console.log(result);
-            res.send({product:result})
+        .then((result) => {
+            res.send({ product: result });
         })
-        .catch((err)=>{
+        .catch((err) => {
             console.error(err);
-            res.send("상품조회시 에러가 발생했습니다.");
-        })
+            res.status(400).send("상품조회시 에러가 발생했습니다.");
+        });
 });
 
-// 기본 형식
-app.get('/', (req, res) => {
-    res.send("Hello!");
-});
-
-// post 방식은 postman에서 설정가능
-app.post('/products', (req, res) => {
+app.post("/products", function (req, res) {
     const body = req.body;
-    // 1. 디스트럭처링으로 상수 body 의 값을 개별적으로 할당
-    const { name, price, seller, description } = body;
-    // if (!name || !price || !seller || !description ){
-    //     res.send("호로롤로로로로로로");
-    // }
-    // 2. 레코드 생성 : Product테이블에 괄호안의 객체를 생성
-    //3. 데이터를 다루는 것은 기본적으로 비동기 통신을 지원하므로 promise 객체 활용
+    const { name, description, price, seller, imageUrl } = body;
+    if (!name || !description || !price || !seller || !imageUrl) {
+        res.send("모든 필드를 입력해주세요");
+    }
     models.Product.create({
         name,
+        description,
         price,
         seller,
-        description
-    }).then((result) => {
-        console.log("상품생성결과", result)
-    }).catch((err) => {
-        console.error(err);
-        res.send("상품업로드에 문제가 발생했습니다.")
+        imageUrl
+    })
+        .then((result) => {
+            console.log("상품생성결과:", result);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(400).send("상품업로드에 문제가 발생했습니다");
+        });
+});
+
+app.post("/image", upload.single("image"), (req, res) => {
+    const file = req.file;
+    console.log(file);
+    res.send({
+        imageUrl: file.path,
+    });
+});
+
+app.get("/banners", (req, res) => {
+    models.Banner.findAll({
+        limit:2,
+    }).then((result)=>{
+        res.send({
+            banner:result,
+        })
+    }).catch((error)=>{
+        res.status(500).send("에러가 발생했습니다",error)
     })
 });
 
 app.listen(port, () => {
-    console.log("뽀삐샵 서버가 구동되고 있습니다.");
+    console.log("뽀삐샵 서버 실행중");
     models.sequelize
         .sync()
         .then(() => {
-            console.log('✓ DB 연결 성공');
+            console.log("DB 연결 성공");
         })
-        .catch(function (err) {
+        .catch((err) => {
+            console.log("DB 연결 실패");
             console.error(err);
-            console.log('✗ DB 연결 에러');
-            // 에러발생시 서버프로세스 종료
             process.exit();
         });
 });
-
-// get 방식
-// 터미널에 node server.js -> 터미널에 뽀삐샵 서버 실행중 출력
-// 웹주소 http://localhost:8080/products -> 브라우저에  res.send 의 내용 출력
---
+++ server_back220716.js
@@ -0,0 +1,108 @@
+// 라우팅은 애플리케이션 엔드 포인트(URI)의 정의, 그리고 URI가 클라이언트 요청에 응답하는 방식
+const express = require("express"); // express 불러오기
+const cors = require('cors'); // cors 불러오기
+const app = express(); // 불러온 express 실생
+const models = require('./models'); // ./models의 함수들을 불러온다.
+const multer = require("multer");
+const port = 8080;
+const upload = multer({ dest: 'uploads/'})  // 83줄 upload 함수작성하고하고 node server.js 하면 여기서 설정한 uploads 이름으로 폴더가생김 포스트맨에서 파일 보내면 여기에 보내짐
+
+//상수 app에
+app.use(express.json()); //json 형식의 데이터를 처리할 수 있게 설정하는 코드
+app.use(cors()) //브라우저의 CORS 이슈를 막기 위해 사용하는 코드
+
+// 다음의 라우트 경로는 요청을 루트 라우트 /에 일치시킴
+app.get('/products', (req, res) => {
+    models.Product.findAll({
+        /* 페이지 갯수 지정
+        limit:1 */
+        // 목록 정렬
+        order:[["createdAt","DESC"]], // 내림차순(최신순) DESC 오름차순 ASC
+        attributes:["id","name","price","seller","description","imageUrl","createdAt"]
+        
+    })
+        .then((result) => {
+            console.log("PRODUCTS:", result);
+            res.send({
+                product: result,
+            });
+        })
+        .catch((err) => {
+            console.error(err);
+            res.send("에러발생");
+        });
+});
+
+app.get('/products/:id', (req, res) => {
+    const params = req.params;
+    const { id } = params;
+    models.Product.findOne({
+        where: {
+            // id: id,
+            id,
+        },
+    })
+        .then((result)=>{
+            console.log(result);
+            res.send({product:result})
+        })
+        .catch((err)=>{
+            console.error(err);
+            res.send("상품조회시 에러가 발생했습니다.");
+        })
+});
+
+// 기본 형식
+app.get('/', (req, res) => {
+    res.send("Hello!");
+});
+
+// app에 post 방식 사용시 요청, 응답 post 방식은 postman에서도 변경
+app.post('/products', (req, res) => {
+    const body = req.body;
+    // 1. 디스트럭처링으로 상수 body 의 값을 개별적으로 할당
+    const { name, price, seller, description } = body;
+    // if (!name || !price || !seller || !description ){
+    //     res.send("호로롤로로로로로로");
+    // }
+    // 2. 레코드 생성 : Product테이블에 괄호안의 객체를 생성
+    //3. 데이터를 다루는 것은 기본적으로 비동기 통신을 지원하므로 promise 객체 활용
+    models.Product.create({
+        name,
+        price,
+        seller,
+        description
+    }).then((result) => {
+        console.log("상품생성결과", result)
+    }).catch((err) => {
+        console.error(err);
+        res.send("상품업로드에 문제가 발생했습니다.")
+    })
+});
+
+app.post("/image",upload.single("image"),(req,res)=>{
+    const file=req.file;
+    console.log(file);
+    res.send({
+        imasgeUrl:file.path,
+    });
+});
+
+app.listen(port, () => {
+    console.log("뽀삐샵 서버가 구동되고 있습니다.");
+    models.sequelize
+        .sync()
+        .then(() => {
+            console.log('✓ DB 연결 성공');
+        })
+        .catch(function (err) {
+            console.error(err);
+            console.log('✗ DB 연결 에러');
+            // 에러발생시 서버프로세스 종료
+            process.exit();
+        });
+});
+
+// get 방식
+// 터미널에 node server.js -> 터미널에 뽀삐샵 서버 실행중 출력
+// 웹주소 http://localhost:8080/products -> 브라우저에  res.send 의 내용 출력
